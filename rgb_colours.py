efcb854890eb37cac856820a644ee55d40c12058
Merry Damsko <merrydamsko@pop-os.localdomain>
2023-07-20 19:47:33 +0200

    issue: object.colormode(x) not recognized in rgb_colours.py

---
+++ rgb_colours.py
@@ -0,0 +1,16 @@
+import turtle as t
+
+
+tim = t.Turtle()
+t.colormode(255)
+
+
+for _ in range(15):
+    tim.forward(10)
+    tim.penup()
+    tim.forward(10)
+    tim.pendown()
+
+
+screen = s
+screen.exitonclick()
